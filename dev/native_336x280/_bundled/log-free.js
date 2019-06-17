d240e7a4dc0c4f070c7f6f863acfc28881824dc8
Gar <gar@lonelydatu.com>
2019-06-17 17:13:13 -0400

    one

--- dev/native_336x280/_bundled/log-free.js
+++ dev/native_336x280/_bundled/log-free.js
@@ -10,7 +10,7 @@ var size = { w: banner.offsetWidth, h: banner.offsetHeight };
 TweenLite.defaultEase = Power2.easeInOut;
 
 var tl = new TimelineMax();
-tl.from('.f2', .5, { opacity: 0, ease: Back.easeOut }, 5);
+tl.from('.f2', .5, { opacity: 0, ease: Power2.easeOut }, 5);
 
 exports.size = size;
 
